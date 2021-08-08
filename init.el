75b4df0f358a64d3ae6aa33b2adbe4120d7c82c2
Bob Sullivan <robertemmettsullivan@gmail.com>
2021-08-08 17:33:11 -0400

    added config for hydra

--- init.el
+++ init.el
@@ -150,3 +150,11 @@
   :after evil
   :config
   (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out"))
+  ("f" nil "finished" :exit t))
