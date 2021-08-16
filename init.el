0c082fbd4a472c5305236dd1bf51dc5a908d5a1c
Bob Sullivan <robertemmettsullivan@gmail.com>
2021-08-16 17:11:57 -0400

    Added config to changes hyphens to dots changed font to Futura

--- init.el
+++ init.el
@@ -211,3 +211,16 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
+(font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Futura" :weight 'regular :height (cdr face)))
